05e6849318a092292d2bfcb606b1b85a3c2dfdc7
André Joarez <andre.ncardoso@hotmail.com>
2022-11-16 10:57:56 -0300

    teste

---
+++ src/components/CalculadoraMonetaria.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-parallax
+    dark
+    src="https://images.pexels.com/photos/36717/amazing-animal-beautiful-beautifull.jpg"
+  >
+    <v-row
+      align="center"
+      justify="center"
+    >
+      <v-col
+        class="text-center"
+        cols="12"
+      >
+        <h1 class="text-h4 font-weight-thin mb-4">
+          Vuetify
+        </h1>
+        <h4 class="subheading">
+          Build your application today!
+        </h4>
+      </v-col>
+    </v-row>
+  </v-parallax>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style scoped>
+
+</style>
